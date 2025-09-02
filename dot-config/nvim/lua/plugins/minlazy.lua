4ff90babbe64748bd1fc52f4f7d3790f9a25dcd1
Wensheng Wang <wensheng.wang@pnmac.com>
2025-09-02 18:39:29 -0500

    up

---
+++ dot-config/nvim/lua/plugins/minlazy.lua
@@ -0,0 +1,22 @@
+return {
+  -- required by copilot
+  -- { "Saghen/blink.cmp", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  -- depends on snacks
+  { "lewis6991/gitsigns.nvim", enabled = false },
+  -- depends on snacks, required by copilot
+  -- { "nvim-lualine/lualine.nvim", enabled = false },
+  { "nvim-mini/mini.ai", enabled = false },
+  { "nvim-mini/mini.icons", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
+  -- nvim-lint 1.04ms  LazyFile
+  -- { "windwp/nvim-ts-autotag", endabled = false },
+  { "folke/persistence.nvim", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+  { "folke/ts-comments.nvim", enabled = false },
+  -- { "folke/which-key.nvim", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "folke/trouble.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+}
