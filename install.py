fb411ac137531e4dcfd1a72633b33890e01c2ae1
Wensheng Wang <wenshengwang@gmail.com>
2025-09-02 21:54:32 -0500

    up

--- install.py
+++ install.py
@@ -63,4 +63,5 @@ for dotconfig in dotconfigs:
     os.symlink(f'{cur_dir}/dot-config/{dotconfig}', dst_dir)
 
 os.system("git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions")
+os.system("git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting")
 
