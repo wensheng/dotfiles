bce03336f00ed9f5ee32396f892d6409e93b6800
Wensheng Wang <wenshengwang@gmail.com>
2026-05-29 16:36:19 -0500

    update nvim config

--- dot-config/nvim/lua/config/autocmds.lua
+++ dot-config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Completely silences the diagnostic UI layer for Markdown buffers
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.diagnostic.enable(false, { bufnr = 0 })
+  end,
+})
